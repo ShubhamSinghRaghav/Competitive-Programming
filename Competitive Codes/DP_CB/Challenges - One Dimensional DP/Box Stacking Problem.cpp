53a73b7451e9016088ac0762132dd1f04b9f30b4
Cheenu <33196651+ShubhamSinghRaghav@users.noreply.github.com>
2020-08-17 02:18:24 +0530

    Update Box Stacking Problem.cpp

--- Competitive Codes/DP_CB/Challenges - One Dimensional DP/Box Stacking Problem.cpp
+++ Competitive Codes/DP_CB/Challenges - One Dimensional DP/Box Stacking Problem.cpp
@@ -1,6 +1,10 @@
 /*
 
-Given a set of N types of rectangular 3-D boxes, where the ith box has height h, width w and length l. Create a stack of boxes which is as tall as possible. However, you can only stack a box on top of another box if the dimensions of the 2-D base of the lower box are each strictly larger than those of the 2-D base of the higher box. Of course, you can rotate a box so that any side functions as its base.It is also allowable to use multiple instances of the same type of box. Your task is to write a program to find the maximum Height by any possible arrangements of the boxes.
+Given a set of N types of rectangular 3-D boxes, where the ith box has height h, width w and length l. 
+Create a stack of boxes which is as tall as possible. However, you can only stack a box on top of 
+another box if the dimensions of the 2-D base of the lower box are each strictly larger than those of the 2-D base of the higher box. 
+Of course, you can rotate a box so that any side functions as its base.It is also allowable to use multiple instances of the same type of box. 
+Your task is to write a program to find the maximum Height by any possible arrangements of the boxes.
 
 Input Format
 The first line contains an integer T denoting the no of test cases.
