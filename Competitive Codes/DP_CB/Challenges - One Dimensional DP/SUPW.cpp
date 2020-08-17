f38006bc75d5bfdfca2cc274bf7aebda8266e97a
Cheenu <33196651+ShubhamSinghRaghav@users.noreply.github.com>
2020-08-17 06:28:21 +0530

    Update SUPW.cpp

--- Competitive Codes/DP_CB/Challenges - One Dimensional DP/SUPW.cpp
+++ Competitive Codes/DP_CB/Challenges - One Dimensional DP/SUPW.cpp
@@ -1,14 +1,14 @@
 /*
 
-In ICO School, all students have to participate regularly in SUPW. There is a different SUPW activity each day, and each activity has its own duration. The SUPW schedule for the next term has been announced, including information about the number of minutes taken by each activity.
-
-Nikhil has been designated SUPW coordinator. His task is to assign SUPW duties to students, including himself. The school's rules say that no student can go three days in a row without any SUPW duty.
-
+In ICO School, all students have to participate regularly in SUPW. 
+There is a different SUPW activity each day, and each activity has its own duration. 
+The SUPW schedule for the next term has been announced, including information about the number of minutes taken by each activity.
+Nikhil has been designated SUPW coordinator. His task is to assign SUPW duties to students, including himself. 
+The school's rules say that no student can go three days in a row without any SUPW duty.
 Nikhil wants to find an assignment of SUPW duty for himself that minimizes the number of minutes he spends overall on SUPW.
 
 Input Format
 A single integer N, the number of days in the future for which SUPW data is available.
-
 N non-negative integers, where the integer in position i represents the number of minutes required for SUPW work on day i.
 
 Constraints
